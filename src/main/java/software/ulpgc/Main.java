f7e697fd66b428b5600948c5671a0f2d362da293
John Doe <juangarciaboissier@gmail.com>
2024-12-01 14:04:57 +0000

    final commit

--- src/main/java/software/ulpgc/Main.java
+++ src/main/java/software/ulpgc/Main.java
@@ -1,5 +1,9 @@
 package software.ulpgc;
 
-public class Main {
+import java.time.LocalDate;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("Juan", LocalDate.of(2004,5,3)));
+    }
 }
--- src/main/java/software/ulpgc/Person.java
+++ src/main/java/software/ulpgc/Person.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class Person {
     private final String name;
     private final LocalDate birthday;
+    private final int DaysInYear=365;
 
     public Person(String name, LocalDate birthday) {
         this.name = name;
@@ -19,4 +20,20 @@ public class Person {
         return birthday;
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", age=" + getAge() +
+                '}';
+    }
+
+    private int getAge() {
+        return toYears(LocalDate.now().toEpochDay()-birthday.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days/DaysInYear);
+    }
 }
